32e500075e2db716c8d4992ce98ff06f15306a54
Dillon Huff <dillonhuff@gmail.com>
2019-02-20 17:53:48 -0800

    Now have something working for generating _hls files

--- tutorial/hw_output.cpp
+++ tutorial/hw_output.cpp
@@ -1,51 +1,308 @@
-#include <cstdio>
-#include <chrono>
-
-//#include "conv_layer.h"
-//#include "conv_layer_auto_schedule.h"
-
-#include "HalideBuffer.h"
 #include "Halide.h"
+#include <stdio.h>
 
 using namespace Halide;
 
+Var x("x"), y("y"), z("z"), c("c");
+Var x_grid("x_grid"), y_grid("y_grid"), xo("xo"), yo("yo"), x_in("x_in"), y_in("y_in");
+uint8_t r_sigma = 32;
+int s_sigma = 8;
+
+class MyPipeline {
+public:
+    ImageParam input;
+    RDom r;
+    Func clamped, input2;
+    Func histogram, downsampled;
+    Func blurx, blury, blurz;
+    Func output, hw_output;
+    std::vector<Argument> args;
+
+    MyPipeline()
+        : input(UInt(8), 2), r(0, s_sigma, 0, s_sigma),
+          input2("input2"), histogram("histogram"), downsampled("downsampled"),
+          blurx("blurx"), blury("blury"), blurz("blurz"),
+          output("output"), hw_output("hw_output")
+    {
+        // Add a boundary condition
+        //clamped = BoundaryConditions::repeat_edge(input);
+        clamped(x, y) = input(x+40, y+40);
+
+        // Construct the bilateral grid
+        Expr val = clamped(x * s_sigma + r.x - s_sigma/2, y * s_sigma + r.y - s_sigma/2);
+        Expr zi = cast<int>((val + r_sigma/2) / r_sigma);
+        histogram(x, y, z, c) = cast<uint16_t>(0);
+        histogram(x, y, zi, c) += select(c == 0, val/16, 64/16);
+
+        // Blur the grid using a five-tap filter
+        blurz(x, y, z, c) = cast<uint16_t>(histogram(x, y, z-2, c) +
+                             histogram(x, y, z-1, c)*4 +
+                             histogram(x, y, z  , c)*6 +
+                             histogram(x, y, z+1, c)*4 +
+                             histogram(x, y, z+2, c)) / 16;
+        blurx(x, y, z, c) = cast<uint16_t>(blurz(x-2, y, z, c) +
+                             blurz(x-1, y, z, c)*4 +
+                             blurz(x  , y, z, c)*6 +
+                             blurz(x+1, y, z, c)*4 +
+                             blurz(x+2, y, z, c)) / 16;
+        blury(x, y, z, c) = cast<uint16_t>(blurx(x, y-2, z, c) +
+                             blurx(x, y-1, z, c)*4 +
+                             blurx(x, y  , z, c)*6 +
+                             blurx(x, y+1, z, c)*4 +
+                             blurx(x, y+2, z, c)) / 16;
+
+
+        // Take trilinear samples to compute the output
+        input2(x, y) = input(x, y);
+        zi = cast<int>(cast<uint16_t>(input2(x, y)) / r_sigma);
+        Expr zf = cast<uint16_t>((cast<uint16_t>(input2(x, y)) % r_sigma)  * (65536 / r_sigma));
+        Expr xf = cast<uint16_t>((x % s_sigma) * (65536 / s_sigma));
+        Expr yf = cast<uint16_t>((y % s_sigma) * (65536 / s_sigma));
+        Expr xi = x/s_sigma;
+        Expr yi = y/s_sigma;
+        Func interpolated("interpolated");
+        interpolated(x, y, c) =
+            lerp(lerp(lerp(blury(xi, yi, zi, c), blury(xi+1, yi, zi, c), xf),
+                      lerp(blury(xi, yi+1, zi, c), blury(xi+1, yi+1, zi, c), xf), yf),
+                 lerp(lerp(blury(xi, yi, zi+1, c), blury(xi+1, yi, zi+1, c), xf),
+                      lerp(blury(xi, yi+1, zi+1, c), blury(xi+1, yi+1, zi+1, c), xf), yf), zf);
+
+        // Normalize
+        val = interpolated(x, y, 0);
+        Expr weight = max(interpolated(x, y, 1), 1); // to avoid underflow
+        hw_output(x, y) = cast<uint8_t>(clamp(val * 64 / weight, 0, 255));
+        output(x, y) = hw_output(x, y);
+
+        // The comment constraints and schedules.
+        //output.bound(x, 0, 1024).bound(y, 0, 1024);
+
+        // Arguments
+        args = {input};
+    }
+
+    void compile_cpu() {
+        std::cout << "\ncompiling cpu code..." << std::endl;
+
+        // The CPU schedule for halide paper
+        blurz.compute_root().reorder(c, z, x, y).parallel(y).vectorize(x, 8).unroll(c);
+        histogram.compute_at(blurz, y);
+        histogram.update().reorder(c, r.x, r.y, x, y).unroll(c);
+        blurx.compute_root().reorder(c, x, y, z).parallel(z).vectorize(x, 8).unroll(c);
+        blury.compute_root().reorder(c, x, y, z).parallel(z).vectorize(x, 8).unroll(c);
+        output.compute_root().parallel(y).vectorize(x, 8);
+
+        //output.print_loop_nest();
+
+        output.compile_to_header("pipeline_native.h", args, "pipeline_native");
+        output.compile_to_object("pipeline_native.o", args, "pipeline_native");
+    }
+
+    void compile_hls() {
+        std::cout << "\ncompiling HLS code..." << std::endl;
+
+        output.tile(x, y, xo, yo, x_in, y_in, 256, 256);
+        output.tile(x_in, y_in, x_grid, y_grid, x_in, y_in, 8, 8);
+
+        blury.store_at(output, xo).compute_at(output, x_grid).reorder(x, y, z, c);
+        blurx.store_at(output, xo).compute_at(output, x_grid).reorder(x, y, z, c);
+        blurz.store_at(output, xo).compute_at(output, x_grid).reorder(z, x, y, c);
+
+        histogram.store_at(output, xo).compute_at(output, x_grid).reorder(c, z, x, y).unroll(c).unroll(z);
+        histogram.update().reorder(c, r.x, r.y, x, y).unroll(c);
+
+        clamped.store_at(output, xo).compute_at(output, x_grid);
+        input2.store_at(output, xo).compute_at(output, x_grid);
+
+        hw_output.compute_at(output, xo);
+        hw_output.accelerate({clamped, input2}, x_grid, xo);
+
+        //output.print_loop_nest();
+
+        output.compile_to_lowered_stmt("pipeline_hls.ir.html", args, HTML);
+        output.compile_to_vhls("pipeline_hls.cpp", args, "pipeline_hls");
+        output.compile_to_header("pipeline_hls.h", args, "pipeline_hls");
+    }
+};
+
+class MyPipelineOpt {
+public:
+    ImageParam input;
+    RDom r;
+    Func clamped;
+    Func input_shuffled, input2_shuffled, output_shuffled;
+    Func histogram, downsampled;
+    Func blurx, blury, blurz;
+    Func output;
+    std::vector<Argument> args;
+
+    MyPipelineOpt()
+        : input(UInt(8), 2), r(0, s_sigma, 0, s_sigma),
+          input_shuffled("input_shuffled"), input2_shuffled("input2_shuffled"),
+          output_shuffled("output_shuffled"),
+          histogram("histogram"), downsampled("downsampled"),
+          blurx("blurx"), blury("blury"), blurz("blurz"),
+          output("output")
+    {
+        // Add a boundary condition
+        //clamped = BoundaryConditions::repeat_edge(input);
+        clamped(x, y) = input(x+40, y+40);
+
+        // shuffle the input
+        input_shuffled(x_in, y_in, x_grid, y_grid)
+            = clamped(x_grid*s_sigma + x_in - s_sigma/2, y_grid*s_sigma + y_in - s_sigma/2);
+
+        // Construct the bilateral grid
+        Expr val = input_shuffled(r.x, r.y, x, y);
+        val = clamp(val, 0, 255);
+        Expr zi = cast<int>((val + r_sigma/2) / r_sigma);
+        histogram(x, y, z, c) = cast<uint16_t>(0);
+        histogram(x, y, zi, c) += select(c == 0, val/16, 64/16);
+
+        // Blur the grid using a five-tap filter
+        blurz(x, y, z, c) = cast<uint16_t>(histogram(x, y, z-2, c) +
+                             histogram(x, y, z-1, c)*4 +
+                             histogram(x, y, z  , c)*6 +
+                             histogram(x, y, z+1, c)*4 +
+                             histogram(x, y, z+2, c)) / 16;
+        blurx(x, y, z, c) = cast<uint16_t>(blurz(x-2, y, z, c) +
+                             blurz(x-1, y, z, c)*4 +
+                             blurz(x  , y, z, c)*6 +
+                             blurz(x+1, y, z, c)*4 +
+                             blurz(x+2, y, z, c)) / 16;
+        blury(x, y, z, c) = cast<uint16_t>(blurx(x, y-2, z, c) +
+                             blurx(x, y-1, z, c)*4 +
+                             blurx(x, y  , z, c)*6 +
+                             blurx(x, y+1, z, c)*4 +
+                             blurx(x, y+2, z, c)) / 16;
+
+
+        // Take trilinear samples to compute the output
+
+        // shuffle the input
+        input2_shuffled(x_in, y_in, x_grid, y_grid)
+            = input(x_grid*s_sigma + x_in, y_grid*s_sigma + y_in);
+
+        Expr sample_val = cast<uint16_t>(input2_shuffled(x_in, y_in, x_grid, y_grid));
+        zi = cast<int>(sample_val / r_sigma);
+        Expr zf = cast<uint16_t>((sample_val % r_sigma) * (65536 / r_sigma));
+        Expr xf = cast<uint16_t>(x_in * (65536 / s_sigma));
+        Expr yf = cast<uint16_t>(y_in * (65536 / s_sigma));
+        Expr xi = x_grid;
+        Expr yi = y_grid;
+        Expr value, weight;
+        value =
+            lerp(lerp(lerp(blury(xi, yi, zi, 0), blury(xi+1, yi, zi, 0), xf),
+                      lerp(blury(xi, yi+1, zi, 0), blury(xi+1, yi+1, zi, 0), xf), yf),
+                 lerp(lerp(blury(xi, yi, zi+1, 0), blury(xi+1, yi, zi+1, 0), xf),
+                      lerp(blury(xi, yi+1, zi+1, 0), blury(xi+1, yi+1, zi+1, 0), xf), yf), zf);
+        weight =
+            lerp(lerp(lerp(blury(xi, yi, zi, 1), blury(xi+1, yi, zi, 1), xf),
+                      lerp(blury(xi, yi+1, zi, 1), blury(xi+1, yi+1, zi, 1), xf), yf),
+                 lerp(lerp(blury(xi, yi, zi+1, 1), blury(xi+1, yi, zi+1, 1), xf),
+                      lerp(blury(xi, yi+1, zi+1, 1), blury(xi+1, yi+1, zi+1, 1), xf), yf), zf);
+        weight = max(weight, 1); // to avoid underflow
+
+        // Normalize
+        output_shuffled(x_in, y_in, x_grid, y_grid) = cast<uint8_t>(clamp(value * 64 / weight, 0, 255));
+
+        // shuffle the output
+        output(x, y) = output_shuffled(x%s_sigma, y%s_sigma, x/s_sigma, y/s_sigma);
+
+        // The comment constraints and schedules.
+        Expr out_width = output.output_buffer().width();
+        Expr out_height = output.output_buffer().height();
+        output
+            .bound(x, 0, (out_width/480)*480)
+            .bound(y, 0, (out_height/640)*640);
+
+        // Arguments
+        args = {input};
+    }
+
+
+    void compile_hls() {
+        std::cout << "\ncompiling HLS code..." << std::endl;
+        output.tile(x, y, xo, yo, x_in, y_in, 480, 640);
+
+        output_shuffled.compute_at(output, xo);
+        input_shuffled.compute_at(output, xo);
+        input2_shuffled.compute_at(output, xo);
+
+        output_shuffled.tile(x_grid, y_grid, xo, yo, x_grid, y_grid, 60, 80);
+        output_shuffled.accelerate({input_shuffled, input2_shuffled}, x_grid, xo);
+
+        blury.linebuffer().reorder(x, y, z, c);
+        blurx.linebuffer().reorder(x, y, z, c);
+        blurz.linebuffer().reorder(z, x, y, c);
+        histogram.linebuffer().reorder(c, z, x, y).unroll(c).unroll(z);
+        histogram.update().reorder(c, r.x, r.y, x, y).unroll(c);
+
+        //output.print_loop_nest();
+
+        // Create the target for HLS simulation
+        Target hls_target = get_target_from_environment();
+        hls_target.set_feature(Target::CPlusPlusMangling);
+        output.compile_to_lowered_stmt("pipeline_hls.ir.html", args, HTML, hls_target);
+        output.compile_to_vhls("pipeline_hls.cpp", args, "pipeline_hls", hls_target);
+        output.compile_to_header("pipeline_hls.h", args, "pipeline_hls", hls_target);
+
+        // Create the Zynq platform target
+        // std::vector<Target::Feature> features({Target::Zynq});
+        // Target target(Target::Linux, Target::ARM, 32, features);
+
+        // output.compile_to_zynq_c("pipeline_zynq.c", args, "pipeline_zynq", target);
+        // output.compile_to_header("pipeline_zynq.h", args, "pipeline_zynq", target);
+
+        // Vectorization and Parallelization Schedules (only work with LLVM codegen)
+        // input_shuffled.vectorize(x_in, 8);
+        // input2_shuffled.vectorize(x_in, 8);
+        // output.vectorize(x_in, 8);
+
+        // output.fuse(xo, yo, xo).parallel(xo);
+
+        // output.compile_to_lowered_stmt("pipeline_zynq.ir.html", args, HTML, target);
+        // output.compile_to_object("pipeline_zynq.o", args, "pipeline_zynq", target);
+        // output.compile_to_assembly("pipeline_zynq.s", args, "pipeline_zynq", target);
+    }
+};
+
 int main(int argc, char **argv) {
+    MyPipeline p1;
+    p1.compile_cpu();
 
-    // We'll define a simple one-stage pipeline:
-    Func brighter;
-    Var x, y;
-
-    // The pipeline will depend on one scalar parameter.
-    Param<uint8_t> offset;
-
-    // And take one grayscale 8-bit input buffer. The first
-    // constructor argument gives the type of a pixel, and the second
-    // specifies the number of dimensions (not the number of
-    // channels!). For a grayscale image this is two; for a color
-    // image it's three. Currently, four dimensions is the maximum for
-    // inputs and outputs.
-    ImageParam input(type_of<uint8_t>(), 2);
-
-    // If we were jit-compiling, these would just be an int and a
-    // Buffer, but because we want to compile the pipeline once and
-    // have it work for any value of the parameter, we need to make a
-    // Param object, which can be used like an Expr, and an ImageParam
-    // object, which can be used like a Buffer.
-
-    // Define the Func.
-    brighter(x, y) = input(x, y) + offset;
-
-    // Schedule it.
-    brighter.vectorize(x, 16).parallel(y);
-
-    // This time, instead of calling brighter.realize(...), which
-    // would compile and run the pipeline immediately, we'll call a
-    // method that compiles the pipeline to a static library and header.
-    //
-    // For AOT-compiled code, we need to explicitly declare the
-    // arguments to the routine. This routine takes two. Arguments are
-    // usually Params or ImageParams.
-    brighter.compile_to_static_library("lesson_10_halide", {input, offset}, "brighter");
-
-    printf("Halide pipeline compiled, but not yet run.\n");
+    MyPipelineOpt p2;
+    p2.compile_hls();
+    return 0;
 }
+
+// #include <cstdio>
+// #include <chrono>
+
+// //#include "conv_layer.h"
+// //#include "conv_layer_auto_schedule.h"
+
+// #include "HalideBuffer.h"
+// #include "Halide.h"
+
+// using namespace Halide;
+
+// int main(int argc, char **argv) {
+
+//     Func brighter;
+//     Var x, y;
+
+//     Param<uint8_t> offset;
+
+//     ImageParam input(type_of<uint8_t>(), 2);
+
+//     brighter(x, y) = input(x, y) + offset;
+
+//     brighter.vectorize(x, 16).parallel(y);
+
+//     Outputs outs;
+//     outs.vhls_source("hw_out.cpp");
+
+//     brighter.compile_to(outs, {input, offset}, "brighter");
+
+//     printf("Halide pipeline compiled, but not yet run.\n");
+// }
