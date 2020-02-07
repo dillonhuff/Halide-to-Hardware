61e6f707bc6df2f5f7bd364f7d06265a57d53412
Dillon Huff <dillonhuff@gmail.com>
2020-02-06 21:24:07 -0800

    More code done?

--- apps/hardware_benchmarks/tests/merged_unit_tests/test_main.cpp
+++ apps/hardware_benchmarks/tests/merged_unit_tests/test_main.cpp
@@ -1678,9 +1678,9 @@ void small_conv_3_3_test() {
 
   conv(x, y) = 0;
 
-  Func hw_input("hw_input"), hw_input_src("hw_input_src");
-  hw_input_src(x, y) = cast<uint16_t>(input(x, y));
-  hw_input(x, y) = cast<uint16_t>(hw_input_src(x, y));
+  Func hw_input("hw_input");
+  //hw_input_src(x, y) = cast<uint16_t>(input(x, y));
+  hw_input(x, y) = cast<uint16_t>(input(x, y));
   conv(x, y)  += kernel(r.x, r.y) * hw_input(x + r.x, y + r.y);
 
   Func hw_output("hw_output");
@@ -1689,7 +1689,7 @@ void small_conv_3_3_test() {
 
   Var xi,yi, xo,yo;
 
-  hw_input_src.compute_root();
+  //hw_input_src.compute_root();
   hw_input.compute_root();
   hw_output.compute_root();
 
@@ -1735,7 +1735,9 @@ void small_conv_3_3_test() {
     .unroll(r.y, 3);
   conv.linebuffer();
 
-  hw_input_src.stream_to_accelerator();
+  //hw_input_src.stream_to_accelerator();
+
+  hw_input.stream_to_accelerator();
 
   // Generate CoreIR
   auto context = hwContext();
@@ -3146,7 +3148,7 @@ int main(int argc, char **argv) {
   //ubuffer_small_conv_3_3_test();
   
   small_conv_3_3_test();
-  assert(false);
+  //assert(false);
   small_conv_3_3_critical_path_test();
   control_path_test();
   control_path_xy_test();
